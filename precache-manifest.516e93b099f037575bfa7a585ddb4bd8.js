0dc861216ddff9b531fbae4873f15012d05265a4
Paul Hiles <paul@devtrends.co.uk>
2020-05-21 20:59:08 -0400

    Updates

---
+++ precache-manifest.516e93b099f037575bfa7a585ddb4bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be98ceb29d231c071b58968c43e7c00c",
+    "url": "/quiz-generator-for-kahoot/index.html"
+  },
+  {
+    "revision": "550ccd65d4cf4817f607",
+    "url": "/quiz-generator-for-kahoot/static/css/main.2a5f19be.chunk.css"
+  },
+  {
+    "revision": "85efddd8357fdffc7dc3",
+    "url": "/quiz-generator-for-kahoot/static/js/2.404bfe99.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/quiz-generator-for-kahoot/static/js/2.404bfe99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550ccd65d4cf4817f607",
+    "url": "/quiz-generator-for-kahoot/static/js/main.d961fe6f.chunk.js"
+  },
+  {
+    "revision": "90befd2563d8c2cf5eca",
+    "url": "/quiz-generator-for-kahoot/static/js/runtime-main.e8f9bde5.js"
+  }
+]);
